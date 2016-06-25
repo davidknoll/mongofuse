9ebc218170e42faad351d547b59f9ad90db7dc05
David Knoll <david@davidknoll.me.uk>
2016-06-25 23:09:24 +0100

    yargs-based command line parsing

--- index.js
+++ index.js
@@ -12,16 +12,42 @@
 // Imports
 var fuse    = require('fuse-bindings');
 var mongojs = require('mongojs');
+var yargs   = require('yargs');
 var mf      = require('./extra.js');
 var ops     = require('./fuseops.js');
 
-// C-isms for commandline args
+// Parse command-line arguments
 // $FlowFixMe: Testing require.main === module is in the Node docs!
 if (require.main === module) {
-  var argv = process.argv.slice(1);
-  var argc = argv.length;
-  var envp = process.env;
-  var returncode = main(argc, argv, envp);
+  var argv = yargs
+    .alias({
+      help:    'h',
+      options: 'o',
+      verbose: 'v',
+      version: 'V'
+    })
+    .count('verbose')
+    .demand(2, 2, 'A MongoDB connection string and a mount point are required. No further options other than the above are accepted.')
+    .describe({
+      options: 'FUSE mount options, comma-separated. See also: http://blog.woralelandia.com/2012/07/16/fuse-mount-options/',
+      verbose: 'Show debugging output (can be specified twice)'
+    })
+    .epilogue('See also: https://www.npmjs.com/package/mongofuse')
+    .example('$0 localhost ~/mongo')
+    .help()
+    .nargs('options', 1)
+    .strict()
+    .string(['_', 'options'])
+    .usage('Usage: $0 <connection-string> <mount-point> [options]')
+    .version()
+    .argv;
+
+  if (Array.isArray(argv.options)) {
+    argv.o = argv.options = argv.options.filter(function (opt) { return typeof opt === 'string'; }).join();
+  }
+  argv.o = argv.options = argv.options.split(',');
+
+  var returncode = main(argv);
   if (returncode) process.exit(returncode);
 }
 
